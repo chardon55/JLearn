239ddd7ef3761ec25d308f0e54524ad43ad59164
dy55 <minidy20001001@outlook.com>
2021-02-04 16:11:41 +0800

    Change theme manager

---
+++ JLearn/Services/JLearnThemeManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Chardon.XamarinThemeManager;
+using Xamarin.Forms;
+using JLearn.Themes;
+
+namespace JLearn.Services
+{
+    public class JLearnThemeManager : ThemeManager
+    {
+        private static readonly ResourceDictionary lightTheme = new LightTheme();
+
+        private static readonly ResourceDictionary darkTheme = new DarkTheme();
+
+        public JLearnThemeManager() : base(lightTheme, darkTheme)
+        {
+        }
+    }
+}
